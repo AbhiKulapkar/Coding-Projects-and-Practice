5a0892dd8ae3b55fd50dd7385b01a9bf55924f2b
AbhishekKulapkar <abhishekkulapkar@gmail.com>
2024-08-30 16:20:31 +0530

    video number 5

--- Practice Problems/checkalphabetsusingconditionaloperator.cpp
+++ Practice Problems/checkalphabetsusingconditionaloperator.cpp
@@ -5,10 +5,7 @@ using namespace std;
 
 int main() {
     char ch;
-    
-    cout << "Enter a character: ";
     cin >> ch;
-    
     (ch >= 'a' && ch <= 'z') || (ch >= 'A' && ch <= 'Z') ? cout << "Alphabet" : cout << "Not an alphabet";
     
     return 0;
---
+++ Practice Problems/checkleapyearusingconditionaloperator.cpp
@@ -0,0 +1,10 @@
+#include<bits/stdc++.h>
+
+using namespace std;
+
+int main() {
+    int year;
+    cin >> year;
+    (year % 4 == 0 && year % 100 != 0) ? cout<<"is leap year"<<year : 
+    (year % 400 == 0) ? cout<<"is leap year" : cout << year <<" is not leap year"<<endl;
+}
