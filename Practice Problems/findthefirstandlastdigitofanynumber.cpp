5bb4c98f31b3bdf4cb0fb50001ef89d1260bce9b
AbhishekKulapkar <abhishekkulapkar@gmail.com>
2024-09-03 14:41:04 +0530

    deleted and addede output.txt

---
+++ Practice Problems/findthefirstandlastdigitofanynumber.cpp
@@ -0,0 +1,16 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+int main(){
+   int num, last;
+   cin >> num;
+   last=num%10;
+   cout<<"the last digit of the entered number is "<<last<<endl;
+
+
+   while(num>=10){
+         num = num/10;
+   }
+    cout<<"the first digit of the entered number is "<<num<<endl;
+   return 0;
+}
