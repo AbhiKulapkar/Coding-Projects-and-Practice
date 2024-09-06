6900796ac69d6b623a79799dde050b5b7fca30f0
AbhishekKulapkar <abhishekkulapkar@gmail.com>
2024-09-06 21:01:15 +0530

    video number 21

---
+++ Practice Problems/addtwointegers.cpp
@@ -0,0 +1,12 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+int main(){
+    int number1, number2, total;
+
+    cin>>number1;
+    cin>>number2;
+    
+    total = number1 + number2;
+    cout<<"total of both number are: "<<total<<endl;
+}
