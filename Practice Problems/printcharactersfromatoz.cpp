032101c07ede0501d41ea549d7b35a47e89c4914
AbhishekKulapkar <abhishekkulapkar@gmail.com>
2024-09-06 20:54:20 +0530

    video number 20

---
+++ Practice Problems/printcharactersfromatoz.cpp
@@ -0,0 +1,16 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+
+int main(){
+    int n =0;
+    char ch = 'A';
+    while(n<26){
+        cout<<ch<<" "<<endl;
+        ch++;
+        n++;
+    }
+
+ return 0;
+    
+}
